361397a707bf667098a10e0850e719a5bce99048
XXXXXX-Diwa <836868609@qq.com>
2020-03-11 23:28:24 +0800

    first commit

--- MF.lua
+++ MF.lua
@@ -1,19 +1,19 @@
-local curspriteRam = 0x030001AC --当前活动块数据起始
-local aranRam = 0x030013D4 --角色数据起始
-local bulletRam = 0x03000A2C --弹丸数据起始
-local aranDecide = 0x030015D8 --角色的判定
-local XScrolloffset = 0x03000144 --水平卷轴偏移start
-local gameMode = 0x03000C70	--检查in-game
-local powerbomeRam = 0x03000130 --PB数据起始
+local curspriteRam = 0x03000140 --当前活动块数据起始
+local aranRam = 0x03001244 --角色数据起始
+local bulletRam = 0x03000960 --弹丸数据起始
+local aranDecide = 0x03001268 --角色的判定
+local XScrolloffset = 0x03000124 --水平卷轴偏移start
+local gameMode = 0x03000BDE	--检查in-game
+local powerbomeRam = 0x03000110 --PB数据起始
 
 local function aran()
 	--根据偏移在屏幕上的坐标
-	local x = (memory.readword(aranRam+0x12)-memory.readword(XScrolloffset))/4
-	local y = (memory.readword(aranRam+0x14)-memory.readword(XScrolloffset+2))/4
+	local x = (memory.readword(aranRam+0x16)-memory.readword(XScrolloffset))/4
+	local y = (memory.readword(aranRam+0x18)-memory.readword(XScrolloffset+4))/4
 	--角色判定框
-	local top = memory.readwordsigned(aranDecide+4)/4
+	local top = memory.readwordsigned(aranDecide+2)/4
 	local left = memory.readwordsigned(aranDecide)/4
-	local right = memory.readword(aranDecide+2)/4
+	local right = memory.readword(aranDecide+4)/4
 	--角色框体坐标
 	local x1 = x + left
 	local y1 = y + top
@@ -24,27 +24,25 @@ end
 	
 local function bullets()
 	for i = 1,0x10 do
-		if memory.readbyte(bulletRam+0x1C*(i-1))~=0 then --仅当子弹有效时
+		if memory.readbyte(bulletRam+0x20*(i-1))~=0 then --仅当子弹有效时
 			--子弹在屏幕上正确的坐标
-			local x = (memory.readword(bulletRam+0xA+0x1C*(i-1))-memory.readword(XScrolloffset))/4
-			local y = (memory.readword(bulletRam+0x8+0x1C*(i-1))-memory.readword(XScrolloffset+2))/4
+			local x = (memory.readword(bulletRam+0xA+0x20*(i-1))-memory.readword(XScrolloffset))/4
+			local y = (memory.readword(bulletRam+0x8+0x20*(i-1))-memory.readword(XScrolloffset+4))/4
 			--弹药大小
-			local top = (memory.readwordsigned(bulletRam+0x14+0x1C*(i-1)))/4
-			local down = (memory.readword(bulletRam+0x16+0x1C*(i-1)))/4
-			local left = (memory.readwordsigned(bulletRam+0x18+0x1C*(i-1)))/4
-			local right = (memory.readword(bulletRam+0x1A+0x1C*(i-1)))/4
+			local top = (memory.readwordsigned(bulletRam+0x16+0x20*(i-1)))/4
+			local down = (memory.readword(bulletRam+0x18+0x20*(i-1)))/4
+			local left = (memory.readwordsigned(bulletRam+0x1A+0x20*(i-1)))/4
+			local right = (memory.readword(bulletRam+0x1C+0x20*(i-1)))/4
 			--子弹框体
 			local x1 = x + left
 			local y1 = y + top
 			local x2 = x + right
 			local y2 = y + down
 			--绘制子弹判定框
-			if memory.readbyte(bulletRam+0xF+0x1C*(i-1))<0xE then --是子弹的话
+			if memory.readbyte(bulletRam+0xF+0x20*(i-1))<0x10 then --仅是子弹时
 				gui.box(x1,y1,x2,y2,"clear","magenta")
-			elseif memory.readbyte(bulletRam+0xF+0x1C*(i-1))~=0xF then --不是超炸的话
-				if memory.readbyte(bulletRam+0x11+0x1C*(i-1))==3 then --爆炸的话
-					gui.box(x1,y1,x2,y2,"clear","purple")
-				end
+			elseif memory.readbyte(bulletRam+0x12+0x20*(i-1))==3 then --炸弹爆炸时
+				gui.box(x1,y1,x2,y2,"clear","purple")
 			end	
 		end
 	end
@@ -55,7 +53,7 @@ local function sprites()
 		if memory.readbyte(curspriteRam+0x38*(i-1))>=3 then --lua难以bit操作,bit位的并,或,故笼统
 			--活动块的坐标
 			local x = (memory.readword(curspriteRam+0x4+0x38*(i-1))-memory.readword(XScrolloffset))/4
-			local y = (memory.readword(curspriteRam+0x2+0x38*(i-1))-memory.readword(XScrolloffset+2))/4
+			local y = (memory.readword(curspriteRam+0x2+0x38*(i-1))-memory.readword(XScrolloffset+4))/4
 			--判定大小
 			local top = (memory.readwordsigned(curspriteRam+0xA+0x38*(i-1)))/4
 			local down = (memory.readword(curspriteRam+0xC+0x38*(i-1)))/4
@@ -67,7 +65,7 @@ local function sprites()
 			local x2 = x + right
 			local y2 = y + down
 			--绘制活动块框体
-			if memory.readbyte(curspriteRam+0x32+0x38*(i-1))>=80 then --是主框体部署的话
+			if memory.readbyte(curspriteRam+0x34+0x38*(i-1))>=80 then --是主框体部署的话
 				gui.box(x1,y1,x2,y2,"clear","orange")
 			else
 				gui.box(x1,y1,x2,y2,"clear","red")
@@ -80,13 +78,13 @@ local function sprites()
 		end
 	end	
 end
-	
+
 local function powerbome()
 	if memory.readword(powerbomeRam)~=0 then
 		if memory.readbyte(powerbomeRam)~=5 then
 			--超炸的坐标
 			local x = (memory.readword(powerbomeRam+0x4)-memory.readword(XScrolloffset))/4
-			local y = (memory.readword(powerbomeRam+0x6)-memory.readword(XScrolloffset+2))/4
+			local y = (memory.readword(powerbomeRam+0x6)-memory.readword(XScrolloffset+4))/4
 			--伤害范围大小
 			local top = (memory.readwordsigned(powerbomeRam+0xC))/4
 			local down = (memory.readword(powerbomeRam+0xE))/4
@@ -101,12 +99,12 @@ local function powerbome()
 			gui.box(x1,y1,x2,y2,"clear","teal")
 		end
 	end
-end	
+end
 	
 while true do
-	if memory.readbyte(gameMode)==4 then
+	if memory.readbyte(gameMode)==1 then
 		aran()
-		bullets()	
+		bullets()
 		sprites()
 		powerbome()
 	end
